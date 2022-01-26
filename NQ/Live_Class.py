6d31a58358e480949c83e724162c09876540ee10
Andres Estrada <aestrad494@gmail.com>
2022-01-25 22:09:21 -0500

    changes...

--- NQ/Live_Class.py
+++ NQ/Live_Class.py
@@ -920,8 +920,10 @@ class Live():
         '''
         allow_margin = False
         order = MarketOrder(action, int(qty)) if ord_type=='market' else StopOrder(action, int(qty), price) if ord_type=='stop' else LimitOrder(action, int(qty), price)
-        try: self.ib.sleep(2); order_data = util.tree(self.ib.whatIfOrder(self.contract, order))['OrderState']; print(order_data)
-        except: self.ib.sleep(2); order_data = util.tree(self.ib.whatIfOrder(self.contract, order))['OrderState']; print(order_data)
+        print(order)
+        print(util.tree(self.ib.whatIfOrder(self.contract, order)))
+        try: self.ib.sleep(2); order_data = util.tree(self.ib.whatIfOrder(self.contract, order))['OrderState']
+        except: self.ib.sleep(2); order_data = util.tree(self.ib.whatIfOrder(self.contract, order))['OrderState']
         # Get margin values
         net_liq = float(order_data['equityWithLoanBefore'])
         margin_req = float(order_data['initMarginAfter'])
