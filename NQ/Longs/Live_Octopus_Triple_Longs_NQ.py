12c1304b24224ea429dc711277628e500b74d16a
Andres Estrada <aestrad494@gmail.com>
2022-04-17 15:02:37 -0500

    erasing symbols

--- NQ/Longs/Live_Octopus_Triple_Longs_NQ.py
+++ NQ/Longs/Live_Octopus_Triple_Longs_NQ.py
@@ -480,3 +480,13 @@ if __name__ == '__main__':
     tempos = [['60', '120'], ['180', '240'], ['540', '720']]
     live_octopus.run_strategy(contracts=6, stop_1=12, target_1=9, target_2=11, trailing_1=0.9, stop_2=35, target_3=29, target_4=66, trailing_2=0.6,
                           stop_3=81, target_5=93, target_6=78, trailing_3=0.9, periods=periods, tempos=tempos, init=init, final=final)
+
+
+# ES
+## LONGS
+# stop_1=3, target_1=2, target_2=3, trailing_1=0.9, stop_2=6, target_3=5, target_4=9, trailing_2=0.9,
+#                          stop_3=18, target_5=12, target_6=20, trailing_3=0.9
+
+## SHORTS
+# stop_1=3, target_1=3, target_2=2, trailing_1=0.9, stop_2=6, target_3=5, target_4=7, trailing_2=0.7,
+#                          stop_3=20, target_5=20, target_6=27, trailing_3=0.9
